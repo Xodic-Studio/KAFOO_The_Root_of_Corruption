bd9ab171a18fcc790a1963b96dc71223d57d2358
MaFIaTH <mafeeyawat@gmail.com>
2023-02-03 17:57:12 +0700

    I'm not going to do this anymore.
    Just fix it for me pls. :( :( :(

--- Assets/Script/MemoryGameSystem.cs
+++ Assets/Script/MemoryGameSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine.UI;
 using UnityEngine;
@@ -7,52 +8,105 @@ using UnityEngine;
 public class MemoryGameSystem : MonoBehaviour
 {
     public Image[] memorySprites;
+    private Animator[] memoryAnimators;
+    public float sequenceInterval = 1f;
+    public int playerNum = 1;
     //0 = w
     //1 = a
     //2 = s
     //3 = d
     [SerializeField] private List<int> sequenceIndices = new List<int>();
+    private List<KeyCode> playerControl = new List<KeyCode>();
     private List<int> userSequence = new List<int>();
-    private bool allowInput = false;
+    [SerializeField] private bool allowInput = false;
+    private static readonly int PlayFeedback = Animator.StringToHash("PlayFeedback");
 
     // Start is called before the first frame update
     void Start()
     {
+        memoryAnimators = new Animator[memorySprites.Length];
+        for (int i = 0; i < memorySprites.Length; i++)
+        {
+            memoryAnimators[i] = memorySprites[i].gameObject.GetComponent<Animator>();
+        }
         RandomSequence(sequenceIndices, 4);
         StartCoroutine(PlaySequence(sequenceIndices));
+        playerControl = playerNum == 1
+            ? new List<KeyCode>() { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D }
+            : new List<KeyCode>() { KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow };
     }
 
     // Update is called once per frame
     void Update()
     {
+        UserInput();
+        UserInputUp();
+    }
+
+    void UserInput()
+    {
         if (allowInput)
         {
             Debug.Log("Input NOW!!!");
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(playerControl[0]))
             {
                 userSequence.Add(0);
-                StartCoroutine(InputCheck(sequenceIndices));
-                allowInput = false;
+                InputCheck(sequenceIndices);
+                ButtonFeedback(0, true);
             }
-            else if (Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(playerControl[1]))
             {
                 userSequence.Add(1);
-                StartCoroutine(InputCheck(sequenceIndices));
-                allowInput = false;
+                InputCheck(sequenceIndices);
+                ButtonFeedback(1, true);
             }
-            else if (Input.GetKeyDown(KeyCode.S))
+            else if (Input.GetKeyDown(playerControl[2]))
             {
                 userSequence.Add(2);
-                StartCoroutine(InputCheck(sequenceIndices));
-                allowInput = false;
+                InputCheck(sequenceIndices);
+                ButtonFeedback(2, true);
             }
-            else if (Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKeyDown(playerControl[3]))
             {
                 userSequence.Add(3);
-                StartCoroutine(InputCheck(sequenceIndices));
-                allowInput = false;
+                InputCheck(sequenceIndices);
+                ButtonFeedback(3, true);
             }
         }
+    }
+
+    void UserInputUp()
+    {
+        if (Input.GetKeyUp(playerControl[0]))
+        {
+            InputCheck(sequenceIndices);
+            ButtonFeedback(0, false);
+        }
+        else if (Input.GetKeyUp(playerControl[1]))
+        {
+            InputCheck(sequenceIndices);
+            ButtonFeedback(1, false);
+        }
+        else if (Input.GetKeyUp(playerControl[2]))
+        {
+            InputCheck(sequenceIndices);
+            ButtonFeedback(2, false);
+        }
+        else if (Input.GetKeyUp(playerControl[3]))
+        {
+            InputCheck(sequenceIndices);
+            ButtonFeedback(3, false);
+        }
+    }
+
+    void ButtonFeedback(int index, bool state)
+    {
+        memoryAnimators[index].SetBool(PlayFeedback, state);
+    }
+
+    void ButtonFeedback(int index)
+    {
+        memoryAnimators[index].SetTrigger("PlayFeedbackTrigger");
     }
 
     IEnumerator PlaySequence(List<int> sequence)
@@ -60,52 +114,42 @@ public class MemoryGameSystem : MonoBehaviour
         allowInput = false;
         foreach (int i in sequence)
         {
-            Color originalColor = memorySprites[i].color;
-            Vector2 originalSize = memorySprites[i].rectTransform.sizeDelta;
-            memorySprites[i].color = Color.green;
-            memorySprites[i].rectTransform.sizeDelta *= 1.25f;
-            yield return new WaitForSeconds(1f);
-            memorySprites[i].color = originalColor;
-            memorySprites[i].rectTransform.sizeDelta = originalSize;
-            yield return new WaitForSeconds(1f);
+            ButtonFeedback(i);
+            yield return new WaitForSeconds(sequenceInterval);
+            ButtonFeedback(i);
         }
         allowInput = true;
     }
 
     void RandomSequence(List<int> sequence, int sequenceLength)
     {
+        sequence.Clear();
         for (int i = 0; i < sequenceLength; i++)
         {
-            sequence.Add(Random.Range(0, 3));
+            sequence.Add(Random.Range(0, 4));
         }
     }
 
-    IEnumerator InputCheck(List<int> sequence)
+    void InputCheck(List<int> sequence)
     {
-        //bool wrongInput = false;
-        while (true)
+        if (CompareSequence(sequence, userSequence) == 0)
         {
-            if (CompareSequence(sequence, userSequence) == 0)
-            {
-                Debug.Log("wrong!!");
-                userSequence.Clear();
-                StartCoroutine(PlaySequence(sequence));
-                yield return null;
-            }
-            else if (CompareSequence(sequence, userSequence) == 1)
-            {
-                Debug.Log("correct but input more!");
-                break;
-            }
-            else
-            {
-                Debug.Log("All correct");
-                yield break;
-            }
-            yield return null;
+            Debug.Log("wrong!!");
+            userSequence.Clear();
+            StartCoroutine(PlaySequence(sequence));
+        }
+        else if (CompareSequence(sequence, userSequence) == 1)
+        {
+            Debug.Log("correct but input more!");
+        }
+        else
+        {
+            Debug.Log("All correct");
+            userSequence.Clear();
+            RandomSequence(sequenceIndices, 4);
+            StartCoroutine(PlaySequence(sequenceIndices));
         }
         allowInput = true;
-        yield return null;
     }
 
     //0 = wrong
@@ -121,11 +165,11 @@ public class MemoryGameSystem : MonoBehaviour
             }
             if (userSequence[i] == sequence[i])
             {
-                if (userSequence.Count == sequence.Count)
+                if (userSequence.Count != sequence.Count)
                 {
-                    return 2;
+                    continue;
                 }
-                return 1;
+                return 2;
             }
         }
         return 1;
