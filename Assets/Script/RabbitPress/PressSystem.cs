eaed959212ca04b4b7eb465597db529e2acf0575
MaFIaTH <mafeeyawat@gmail.com>
2023-02-04 21:08:56 +0700

    TANK Particle

--- Assets/Script/RabbitPress/PressSystem.cs
+++ Assets/Script/RabbitPress/PressSystem.cs
@@ -9,12 +9,20 @@ using Random = UnityEngine.Random;
 
 public class PressSystem : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
     [SerializeField] private float changeKeyRate;
     [SerializeField] private int keyLimit;
     [SerializeField] private KeyCode key;
     [SerializeField] private List<Sprite> keyImage;
     [SerializeField] private List<KeyCode> keys;
     [SerializeField] public int pressGoal;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private GameObject[] particleObjects;
+    [SerializeField] private int spawnCount;
+    [SerializeField] private float[] xRange, yRange;
+    [SerializeField] private bool comboParticlePlayed;
+    [SerializeField] private ParticleSystem particleSystem;
+    [SerializeField] private AudioSource audioSource;
     [HideInInspector] public int pressCount;
     public TimeSystem timeSystem;
     
@@ -24,6 +32,7 @@ public class PressSystem : MonoBehaviour
 
     private void Start()
     {
+        cam = Camera.main;
         otherKey = keys.ToList();
         otherKey.Remove(key);
     }
@@ -65,13 +74,41 @@ public class PressSystem : MonoBehaviour
                 Debug.Log("Change Key");
                     
             }
+            particleSystem.Play();
             pressCount++;
+            comboParticlePlayed = false;
         }
 
         if (Input.GetKeyUp(key) || Input.GetKeyUp(keyBefore))
         {
             GetComponent<Image>().color = Color.white;
         }
+        PlayComboEffect();
+    }
+
+    private void PlayComboEffect()
+    {
+        
+        if (pressCount % 5 == 0 && pressCount > 0 && !comboParticlePlayed)
+        {
+            comboParticlePlayed = true;
+            //audioSource.Play();
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Vector2 position = cam.ScreenToWorldPoint(spawnPosition.position);
+                GameObject spawned = Instantiate(particleObjects[Random.Range(0, particleObjects.Length)],
+                    position, Quaternion.identity);
+                spawned.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(xRange[0], xRange[1]),
+                    Random.Range(yRange[0], yRange[1])));
+                StartCoroutine(DestroyObject(spawned));
+            }
+        }
+    }
+
+    private IEnumerator DestroyObject(GameObject gameObjectToDestroy)
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObjectToDestroy);
     }
 
     private void TurnOnPress()
